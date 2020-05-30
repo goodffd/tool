3b8c3f0b99b488e12b8e56ae7bbb8ea76ba2a230
goodffd <62946026+goodffd@users.noreply.github.com>
2020-05-30 22:16:24 +0800

    Update server-confs.sh

--- server-confs.sh
+++ server-confs.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
 common() {
-iptables -A INPUT -s 27.122.57.247 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 92.38.189.201 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 195.133.197.58 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 103.72.4.233 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 89.208.253.8 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j DROP
+      if ! iptables -C INPUT -s 27.122.57.247 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 27.122.57.247 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -s 92.38.189.201 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 92.38.189.201 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -s 195.133.197.58 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 195.133.197.58 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -s 103.72.4.233 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 103.72.4.233 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -s 89.208.253.8 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 89.208.253.8 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT; then
+           iptables -A INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT
+      fi
+      if ! iptables -C INPUT -i lo -j ACCEPT; then
+           iptables -A INPUT -i lo -j ACCEPT
+      fi
+      if ! iptables -C INPUT -p tcp --dport 22 -j DROP; then
+           iptables -A INPUT -p tcp --dport 22 -j DROP
+      fi
 }
 
 common &
