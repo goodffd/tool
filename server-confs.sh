57d560467da501d59856f5c86ae5e43af57d5eca
goodffd <62946026+goodffd@users.noreply.github.com>
2020-06-12 22:53:28 +0800

    Update server-confs.sh

--- server-confs.sh
+++ server-confs.sh
@@ -14,8 +14,8 @@ common() {
       if ! iptables -C INPUT -s 103.72.4.233 -p tcp --dport 22 -j ACCEPT; then
            iptables -A INPUT -s 103.72.4.233 -p tcp --dport 22 -j ACCEPT
       fi
-      if ! iptables -C INPUT -s 89.208.253.8 -p tcp --dport 22 -j ACCEPT; then
-           iptables -A INPUT -s 89.208.253.8 -p tcp --dport 22 -j ACCEPT
+      if ! iptables -C INPUT -s 89.208.253.8 -p tcp --dport 10086 -j ACCEPT; then
+           iptables -A INPUT -s 89.208.253.8 -p tcp --dport 10086 -j ACCEPT
       fi
       if ! iptables -C INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT; then
            iptables -A INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT
