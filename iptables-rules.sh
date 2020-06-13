671a290b5593dafec61304f6c7d7b3295fb70af8
goodffd <62946026+goodffd@users.noreply.github.com>
2020-06-13 17:59:30 +0800

    Update iptables-rules.sh

--- iptables-rules.sh
+++ iptables-rules.sh
@@ -17,12 +17,6 @@
       if ! iptables -C INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT; then
            iptables -A INPUT -s 154.17.2.166 -p tcp --dport 22 -j ACCEPT
       fi
-      if ! iptables -C INPUT -s 151.248.68.132 -p tcp --dport 22 -j ACCEPT; then
-           iptables -A INPUT -s 151.248.68.132 -p tcp --dport 22 -j ACCEPT
-      fi
-      if ! iptables -C INPUT -s 185.172.113.241 -p tcp --dport 22 -j ACCEPT; then
-           iptables -A INPUT -s 185.172.113.241 -p tcp --dport 22 -j ACCEPT
-      fi
       if ! iptables -C INPUT -i lo -j ACCEPT; then
            iptables -A INPUT -i lo -j ACCEPT
       fi
