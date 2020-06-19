b08fa1d6ff3f61243d33e244785da005d0de219c
goodffd <62946026+goodffd@users.noreply.github.com>
2020-06-19 17:23:01 +0800

    Update server-confs.sh

--- server-confs.sh
+++ server-confs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 common() {
-      sleep 5
+    while [ ! iptables -C INPUT -p tcp --dport 22 -j DROP ]; do
       bash <(curl -s https://raw.githubusercontent.com/goodffd/tool/master/iptables-rules.sh)
+    done
 }
 
 common &
-
 sleep infinity
