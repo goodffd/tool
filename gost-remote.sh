bf2e3eb2188ce83312c7b9fa6248e9e4c3c60c0a
goodffd <62946026+goodffd@users.noreply.github.com>
2020-06-20 00:07:49 +0800

    Update gost-remote.sh

--- gost-remote.sh
+++ gost-remote.sh
@@ -28,16 +28,63 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
   release="centos"
   systemPackage="yum"
-
 fi
-${sudoCmd} ${systemPackage} install wget -y -qq
-${sudoCmd} systemctl disable gost.service
-${sudoCmd} wget -q -N https://raw.githubusercontent.com/goodffd/tool/master/gost.service -O /etc/systemd/system/gost.service
+
+create_gost_config() {
 if [ ! -d "/etc/gost" ]; then
   mkdir /etc/gost
-  ${sudoCmd} wget -q -N https://raw.githubusercontent.com/goodffd/tool/master/gost-remote-config.json -O /etc/gost/config.json
-else
-  ${sudoCmd} wget -q -N https://raw.githubusercontent.com/goodffd/tool/master/gost-remote-config.json -O /etc/gost/config.json
 fi
+${sudoCmd} cat > /etc/gost/config.json <<-EOF
+{
+    "Debug": false,
+    "Retries": 0,
+    "ServeNodes": [],
+    "ChainNodes": [],
+    "Routes": [
+        {
+            "ServeNodes": [ "relay+tls://:13001/127.0.0.1:32357" ],
+            "ChainNodes": []
+        }
+    ]
+}
+EOF
+}
+
+create_gost_service(){
+${sudoCmd} cat > /etc/systemd/system/gost.service <<-EOF
+[Unit]
+Description=gost service
+After=network.target network-online.target nss-lookup.target
+Wants=network-online.target
+
+[Service]
+ExecStart=/usr/bin/gost -C /etc/gost/config.json
+Restart=on-failure
+
+[Install]
+WantedBy=default.target
+EOF
+}
+
+get_gost(){
+  if [ ! -f "/usr/bin/gost" ]; then
+      local API_URL="https://api.github.com/repos/ginuerzh/gost/releases/latest"
+      local DOWNLOAD_URL="$(curl -H "Accept: application/json" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0" -s "${API_URL}" --connect-timeout 10| grep 'browser_download_url' | grep 'linux-amd64' | cut -d\" -f4)"
+      ${sudoCmd} curl -L -H "Cache-Control: no-cache" -o "/tmp/gost.gz" "${DOWNLOAD_URL}"
+      ${sudoCmd} gzip -d /tmp/gost.gz
+      ${sudoCmd} mv /tmp/gost /usr/bin/gost
+      ${sudoCmd} chmod +x /usr/bin/gost
+  fi
+}
+
+${sudoCmd} ${systemPackage} install curl gzip -y -qq
+${sudoCmd} systemctl stop gost.service
+${sudoCmd} systemctl disable gost.service
+${sudoCmd} rm -f /etc/systemd/system/gost.service
+${sudoCmd} rm -f /etc/systemd/system/gost.service
+${sudoCmd} rm -f /etc/gost/config.json
+get_gost
+create_gost_config
+create_gost_service
 ${sudoCmd} systemctl enable gost.service
 ${sudoCmd} systemctl restart gost.service
