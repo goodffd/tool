63ab18979a82293b226e892dbc7c4ce12ff973f7
goodffd <62946026+goodffd@users.noreply.github.com>
2020-06-19 22:49:27 +0800

    Update gost-forward.sh

--- gost-forward.sh
+++ gost-forward.sh
@@ -30,19 +30,73 @@ elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
   systemPackage="yum"
 
 fi
-${sudoCmd} ${systemPackage} install wget -y -qq
+
+create_gost_config() {
+if [ ! -d "/etc/gost" ]; then
+  mkdir /etc/gost
+fi
+${sudoCmd} cat > /etc/gost/config.json <<-EOF
+{
+    "Debug": false,
+    "Retries": 0,
+    "ServeNodes": [],
+    "ChainNodes": [],
+    "Routes": [
+        {
+            "ServeNodes": [ "tcp://:13001" ],
+            "ChainNodes": [ "relay+tls://103.72.4.233:13001" ]
+        },
+        {
+            "ServeNodes": [ "udp://:13001" ],
+            "ChainNodes": [ "relay+mtls://103.72.4.233:13001" ]
+        },
+        {
+            "ServeNodes": [ "tcp://:13002" ],
+            "ChainNodes": [ "relay+tls://92.38.189.201:13002" ]
+        },
+        {
+            "ServeNodes": [ "udp://:13002" ],
+            "ChainNodes": [ "relay+mtls://92.38.189.201:13002" ]
+        }
+
+    ]
+}
+EOF
+}
+
+create_gost_service(){
+${sudoCmd} cat > /etc/systemd/system/gost.service <<-EOF
+[Unit]
+Description=gost service
+After=network.target network-online.target nss-lookup.target
+Wants=network-online.target
+
+[Service]
+ExecStart=/usr/bin/gost -C /etc/gost/config.json
+Restart=on-failure
+
+[Install]
+WantedBy=default.target
+EOF
+}
+
+get_gost(){
+  if [ ! -f "/usr/bin/gost" ]; then
+      ${sudoCmd} wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
+      ${sudoCmd} gzip -d gost-linux-amd64-2.11.1.gz
+      ${sudoCmd} mv gost-linux-amd64-2.11.1 /usr/bin/gost
+      ${sudoCmd} chmod +x /usr/bin/gost
+  fi
+}
+
+${sudoCmd} ${systemPackage} install wget gzip -y -qq
 ${sudoCmd} systemctl stop gost.service
 ${sudoCmd} systemctl disable gost.service
 ${sudoCmd} rm -f /etc/systemd/system/gost.service
 ${sudoCmd} rm -f /etc/systemd/system/gost.service
-while [ ! -f "/etc/systemd/system/gost.service" ]; do
-    ${sudoCmd} wget -q -N https://raw.githubusercontent.com/goodffd/tool/master/gost.service -O /etc/systemd/system/gost.service
-done
-if [ ! -d "/etc/gost" ]; then
-  mkdir /etc/gost
-fi
-while [ ! -f "/etc/gost/config.json" ]; do
-  ${sudoCmd} wget -q -N https://raw.githubusercontent.com/goodffd/tool/master/gost-forward-config.json -O /etc/gost/config.json
-done
+${sudoCmd} rm -f /etc/gost/config.json
+get_gost
+create_gost_config
+create_gost_service
 ${sudoCmd} systemctl enable gost.service
 ${sudoCmd} systemctl restart gost.service
