bbd82e4f8804a61c5b92f475d51692d980384ef4
goodffd <62946026+goodffd@users.noreply.github.com>
2020-07-26 17:07:44 +0800

    Update gfwlist2ros_rules.sh

--- gfwlist2ros_rules.sh
+++ gfwlist2ros_rules.sh
@@ -44,26 +44,34 @@ nginx_root="/usr/share/nginx/html"
 rm -f ${nginx_root}/gfwlist_ip.rsc
 
 #解析gfwlist域名并验证解析结果是否为合法的ip地址
-while read -r line
-do
-  #将读取的每一行域名删除回车符、换行符
-  line=`echo ${line} | tr -d '\n' | tr -d '\r'`
-  #取dig answer段的最后一行解析结果（解析出来如果是有CNAME记录和ip记录，则ip记录是在最后行）
-  ip=`dig ${line} +short | tail -n 1`
-  #用ipcalc验证ip地址合法性（如果dig的结果为非ip地址，如CNAME，则判定为非合法的ip地址）
-  #ipcalc只适用centos，其他系统用脚本判断（脚本判断耗时为ipcalc的3倍左右）
-  if [ ${release} == "centos" ]; then
-     ipcalc -cs ${ip}
-       if [ $? -eq 0 ]; then
-         echo ${ip} >> ${nginx_root}/gfwlist_ip.rsc
-       fi
-   else 
-      VALID_CHECK=$(echo $ip|awk -F. '$1<=255&&$2<=255&&$3<=255&&$4<=255{print "yes"}')
-        if [ ${VALID_CHECK:-no} == "yes" ]; then
-            echo ${ip} >> ${nginx_root}/gfwlist_ip.rsc
-        fi
-   fi
-done < gfwlist_domain.rsc
+#用ipcalc验证ip地址合法性（如果dig的结果为非ip地址，如CNAME，则判定为非合法的ip地址）
+#ipcalc只适用centos，其他系统用脚本判断（脚本判断耗时为ipcalc的3倍左右）
+if [ ${release} == "centos" ]; then
+    while read -r line
+    do
+      #将读取的每一行域名删除回车符、换行符
+      line=`echo ${line} | tr -d '\n' | tr -d '\r'`
+      #取dig answer段的最后一行解析结果（解析出来如果是有CNAME记录和ip记录，则ip记录是在最后行）
+      ip=`dig ${line} +short | tail -n 1`
+      ipcalc -cs ${ip}
+           if [ $? -eq 0 ]; then
+             echo ${ip} >> ${nginx_root}/gfwlist_ip.rsc
+           fi
+     done < gfwlist_domain.rsc
+else 
+     while read -r line
+     do
+       #将读取的每一行域名删除回车符、换行符
+       line=`echo ${line} | tr -d '\n' | tr -d '\r'`
+       #取dig answer段的最后一行解析结果（解析出来如果是有CNAME记录和ip记录，则ip记录是在最后行）
+       ip=`dig ${line} +short | tail -n 1`
+       #其他系统用脚本判断
+       VALID_CHECK=$(echo $ip|awk -F. '$1<=255&&$2<=255&&$3<=255&&$4<=255{print "yes"}')
+           if [ ${VALID_CHECK:-no} == "yes" ]; then
+              echo ${ip} >> ${nginx_root}/gfwlist_ip.rsc
+           fi
+      done < gfwlist_domain.rsc
+fi
 
 sort -n ${nginx_root}/gfwlist_ip.rsc | uniq > ${nginx_root}/gfwlist_ip_finall.rsc
 
