0e8798ae1623d47560c5f9e8015396befef85725
goodffd <62946026+goodffd@users.noreply.github.com>
2020-07-26 18:50:10 +0800

    Update gretunnel4centos.sh

--- gretunnel4centos.sh
+++ gretunnel4centos.sh
@@ -197,12 +197,12 @@ ${sudoCmd} ${systemPackage} install -y iptables-services
 ${sudoCmd} systemctl enable iptables.service
 
 is_exist=$(iptables-save | grep -- "-A POSTROUTING -o eth0 -j MASQUERADE")
-if [ -z ${is_exit} ]; then
+if [ -z ${is_exist} ]; then
     ${sudoCmd} iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
 fi
 
 is_exist=$(iptables-save | grep -- "-A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu")
-if [ -z ${is_exit} ]; then
+if [ -z ${is_exist} ]; then
     ${sudoCmd} iptables -t mangle -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 fi
 ${sudoCmd} service iptables save
