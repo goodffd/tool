ab39700bb42ce613b39fe66b948480794d0509ac
goodffd <62946026+goodffd@users.noreply.github.com>
2020-08-09 14:49:21 +0800

    Update gretunnel4ubuntuR.sh

--- gretunnel4ubuntuR.sh
+++ gretunnel4ubuntuR.sh
@@ -191,12 +191,12 @@ ${sudoCmd} cat > /etc/network-conf.sh <<-"EOF"
 common() {
       is_exist=$(iptables-save | grep -- "-A POSTROUTING -o eth0 -j MASQUERADE")
       if [[ -z "${is_exist}" ]]; then
-          ${sudoCmd} iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+          iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
       fi
 
       is_exist=$(iptables-save | grep -- "-A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu")
       if [[ -z "${is_exist}" ]]; then
-          ${sudoCmd} iptables -t mangle -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+          iptables -t mangle -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
       fi
       local_ip=`ifconfig -a|grep -o -e 'inet [0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}'|grep -v "127.0.0"|awk '{print $2}'| head -n 1`
       remote_ip=$(dig ipv4.fclouds.xyz @1.1.1.1 +short)
