231e915278eaf59425be050e5af1d63caf9cceef
goodffd <62946026+goodffd@users.noreply.github.com>
2020-08-15 19:54:05 +0800

    Update cnlist2ros.sh

--- cnlist2ros.sh
+++ cnlist2ros.sh
@@ -47,18 +47,20 @@ echo "172.16.0.0/12" >> ${cn_filename}
 echo "10.0.0.0/8" >> ${cn_filename}
 
 #开始处理 cn_filename 内容
-# cn_filename1
+#方法1
+sed -i 's/\(.*\)/add address=\1 list=CN/g;1 i/log info "Loading CN ipv4 address list";2 i/ip firewall address-list remove [/ip firewall address-list find list=CN];3 i/ip firewall address-list' ${gfwlist_domain_filename}
+#方法2
 #1、每行行首增加字符串"add address="
-sed -i 's/^/add address=&/g' ${cn_filename}
+#sed -i 's/^/add address=&/g' ${cn_filename}
 
 #2、每行行尾增加字符串" list=CN"
-sed -i 's/$/& list=CN/g' ${cn_filename}
+#sed -i 's/$/& list=CN/g' ${cn_filename}
 
 #3、在文件第1行前插入新行"/log info "Loading CN ipv4 address list""
-sed -i '1 i/log info "Loading CN ipv4 address list"' ${cn_filename}
+#sed -i '1 i/log info "Loading CN ipv4 address list"' ${cn_filename}
 
 #4、在文件第2行前插入新行"/ip firewall address-list remove [/ip firewall address-list find list=CN]"
-sed -i '2 i/ip firewall address-list remove [/ip firewall address-list find list=CN]' ${cn_filename}
+#sed -i '2 i/ip firewall address-list remove [/ip firewall address-list find list=CN]' ${cn_filename}
 
 #5、在文件第3行前插入新行"/ip firewall address-list"
-sed -i '3 i/ip firewall address-list' ${cn_filename}
+#sed -i '3 i/ip firewall address-list' ${cn_filename}
