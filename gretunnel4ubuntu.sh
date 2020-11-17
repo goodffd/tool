d35244727b40b2b9cec9144c8ace7df356ade672
goodffd <62946026+goodffd@users.noreply.github.com>
2020-11-17 22:33:20 +0800

    Update gretunnel4ubuntu.sh

--- gretunnel4ubuntu.sh
+++ gretunnel4ubuntu.sh
@@ -266,4 +266,4 @@ _green 'install iptables & nat masquerdo & Change MSS & gretunnel load at start.
 #_green 'cron ddns scripts...done.\n'
 echo "*/1 * * * * ping ${gre_ip_peer} -c5 >> /var/log/grekeepalive.log 2>&1" >> /var/spool/cron/crontabs/root
 echo "0 0 * * 1 rm -f /var/log/grekeepalive.log" >> /var/spool/cron/crontabs/root
-_yellow 'cron scripts...done.\n'
+_green 'cron scripts...done.\n'
