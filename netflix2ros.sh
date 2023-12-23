dc23891f9047e7ef4b1cfa043718e2bb6614897f
goodffd <62946026+goodffd@users.noreply.github.com>
2023-12-23 17:02:32 +0800

    Update netflix2ros.sh

--- netflix2ros.sh
+++ netflix2ros.sh
@@ -63,9 +63,9 @@ ${sudoCmd} sed -i 's/^/\/ip firewall layer7-protocol set [find name="netflix"] r
 #${sudoCmd} sed -i '3 i/ip dns static' Netflix.list.rosdns
 
 #生成ros dns v7.6之后
-${sudoCmd} sed 's/\(.*\)/:do { add name="\1" type=FWD forward-to=$netflix match-subdomain=yes comment=Netflix } on-error={}/g' netflix_d.txt > netflix.dns
-${sudoCmd} sed 's/\(.*\)/:do { add name="\1" type=FWD forward-to=$netflix match-subdomain=yes comment=Netflix } on-error={}/g' netflix_ds.txt >> netflix.dns
-${sudoCmd} sed 's/\(.*\)/:do { add regexp="\1" type=FWD forward-to=$netflix comment=Netflix } on-error={}/g' netflix_dk.txt >> netflix.dns
+${sudoCmd} sed 's/\(.*\)/:do { add name="\1" type=FWD forward-to=$netflix match-subdomain=yes address-list=netflix comment=Netflix } on-error={}/g' netflix_d.txt > netflix.dns
+${sudoCmd} sed 's/\(.*\)/:do { add name="\1" type=FWD forward-to=$netflix match-subdomain=yes address-list=netflix comment=Netflix } on-error={}/g' netflix_ds.txt >> netflix.dns
+${sudoCmd} sed 's/\(.*\)/:do { add regexp="\1" type=FWD forward-to=$netflix address-list=netflix comment=Netflix } on-error={}/g' netflix_dk.txt >> netflix.dns
 ${sudoCmd} sed -i '1 i:local netflix 192.168.99.1' netflix.dns
 ${sudoCmd} sed -i '2 i/ip dns static remove [/ip dns static find comment="Netflix"]' netflix.dns
 ${sudoCmd} sed -i '3 i/ip dns static' netflix.dns
