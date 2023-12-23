448c4c61c4c34af1c131cd9c2fcaea3a18348f60
goodffd <62946026+goodffd@users.noreply.github.com>
2023-12-23 11:11:28 +0800

    Update cndomain.sh

--- cndomain.sh
+++ cndomain.sh
@@ -38,7 +38,7 @@ fi
 
 wget -N --no-check-certificate -O cndomain.txt https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/ChinaMax/ChinaMax_Domain.list
 
-cn_domain_filename="cndomain2ros.rsc"
+cn_domain_filename="cndomain.txt"
 
 #开始处理 cn_domain_filename 内容
 sed -i '/^#/d' ${cn_domain_filename}
