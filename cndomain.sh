f80118e33875f3e3065fe252782a3a51105e8013
goodffd <62946026+goodffd@users.noreply.github.com>
2023-12-23 11:04:16 +0800

    Update and rename cndomain.txt to cndomain.sh

---
+++ cndomain.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  sudoCmd="sudo"
+else
+  sudoCmd=""
+fi
+
+#copied & modified from atrandys trojan scripts
+#copy from 秋水逸冰 ss scripts
+if [[ -f /etc/redhat-release ]]; then
+  release="centos"
+  systemPackage="yum"
+elif cat /etc/issue | grep -Eqi "debian"; then
+  release="debian"
+  systemPackage="apt-get"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+  release="ubuntu"
+  systemPackage="apt-get"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+  release="centos"
+  systemPackage="yum"
+elif cat /proc/version | grep -Eqi "debian"; then
+  release="debian"
+  systemPackage="apt-get"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+  release="ubuntu"
+  systemPackage="apt-get"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+  release="centos"
+  systemPackage="yum"
+fi
+
+if [ ${systemPackage} == "yum" ]; then
+    ${sudoCmd} ${systemPackage} install wget -y -q
+else
+    ${sudoCmd} ${systemPackage} install wget -y -qq
+fi
+
+wget -N --no-check-certificate -O cndomain.txt https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/ChinaMax/ChinaMax_Domain.list
+
+cn_domain_filename="cndomain2ros.rsc"
+
+#开始处理 cn_domain_filename 内容
+sed -i '/^#/d' ${cn_domain_filename}
+sed -i '/^\./ s/^.//; s/\(.*\)/domain:\1/g' ${cn_domain_filename}
