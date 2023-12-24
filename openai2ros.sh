ad686ee4bbe57321654255cb25d99dc078860e42
goodffd <62946026+goodffd@users.noreply.github.com>
2023-12-25 01:05:26 +0800

    Update openai2ros.sh

--- openai2ros.sh
+++ openai2ros.sh
@@ -38,13 +38,23 @@ fi
 
 ${sudoCmd} wget -O OpenAI.list https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/OpenAI/OpenAI.list
 ${sudoCmd} sed -rn 's/^DOMAIN-SUFFIX,(.*)/\1/p' OpenAI.list > openai_ds.txt
-${sudoCmd} sed -rn 's/^DOMAIN-KEYWORD,(.*)/.*\1.*/p' OpenAI.list > openai_dk.txt
+${sudoCmd} sed -rn 's/^DOMAIN-KEYWORD,(.*)/\1/p' OpenAI.list > openai_dk.txt
 ${sudoCmd} sed -rn '/^DOMAIN-SUFFIX,|^DOMAIN-KEYWORD,/!s/^DOMAIN,(.*)/\1/p' OpenAI.list > openai_d.txt
 
 
 #生成ros L7
-${sudoCmd} cat openai_d.txt openai_ds.txt openai_dk.txt > openai.txt
-${sudoCmd} sort openai.txt | uniq > openai.rosL7
+${sudoCmd} sort openai_ds.txt | uniq > openai_ds.rosL7
+${sudoCmd} sed -i ':a;N;s/\n/|/g;ta' openai_ds.rosL7
+${sudoCmd} sed -i 's/\(.*\)/(\\\\.|^)(\1)\\$/g' openai_ds.rosL7
+
+${sudoCmd} sort openai_dk.txt | uniq > openai_dk.rosL7
+${sudoCmd} sed -i ':a;N;s/\n/|/g;ta' openai_dk.rosL7
+
+${sudoCmd} sort openai_d.txt | uniq > openai_d.rosL7
+${sudoCmd} sed -i ':a;N;s/\n/|/g;ta' openai_d.rosL7
+${sudoCmd} sed -i 's/\(.*\)/\^(\1)\\$/g' openai_d.rosL7
+
+${sudoCmd} cat openai_d.rosL7 openai_ds.rosL7 openai_dk.rosL7 > openai.rosL7
 ${sudoCmd} sed -i ':a;N;s/\n/|/g;ta' openai.rosL7
 ${sudoCmd} sed -i 's/^/"/g;s/$/"/g' openai.rosL7
 ${sudoCmd} sed -i 's/^/\/ip firewall layer7-protocol set [find name="openai"] regexp=/g' openai.rosL7
